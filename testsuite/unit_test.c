a4e84005b6329e2e180551b2f88477adff74c0b2
Fabian van der Werf <fvanderwerf@gmail.com>
2015-10-26 20:29:21 +0100

    add more ge unittests

--- testsuite/unit_test.c
+++ testsuite/unit_test.c
@@ -12,16 +12,84 @@
 void test_ge()
 {
     FAB_GE();
-    fail("GE");
+    fail(__func__);
     return;
 error:
-    pass("GE");
+    pass(__func__);
+    return;
+}
+
+void test_cge_true()
+{
+    FAB_CGE(1);
+    fail(__func__);
+    return;
+error:
+    pass(__func__);
+    return;
+}
+
+void test_cge_false()
+{
+    FAB_CGE(0);
+    pass(__func__);
+    return;
+error:
+    fail(__func__);
+    return;
+}
+
+void test_cge_null_null()
+{
+    FAB_CGE_NULL(NULL);
+    fail(__func__);
+    return;
+error:
+    pass(__func__);
+    return;
+}
+
+void test_cge_null_nonnull()
+{
+    FAB_CGE_NULL((int *) 1);
+    pass(__func__);
+    return;
+error:
+    fail(__func__);
+    return;
+}
+
+void test_cge_neg_neg()
+{
+    FAB_CGE_NEG(-1);
+    fail(__func__);
+    return;
+error:
+    pass(__func__);
+    return;
+}
+
+void test_cge_neg_nonneg()
+{
+    FAB_CGE_NEG(0);
+    FAB_CGE_NEG(1);
+    pass(__func__);
+    return;
+error:
+    fail(__func__);
     return;
 }
 
 int main(int argc, char *argv[])
 {
     test_ge();
+    test_cge_true();
+    test_cge_false();
+    test_cge_null_null();
+    test_cge_null_nonnull();
+    test_cge_neg_neg();
+    test_cge_neg_nonneg();
+
     totals();
     exit(EXIT_SUCCESS);
 }
